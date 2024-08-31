f7e351ab5b28a12085722e2a922d6331ae54cc54
Prasada Indukuri <prasada.indukuri_migration@specialized.com>
2024-08-31 16:23:40 +1000

    link to YouTube channel

--- components/smallfooter.tsx
+++ components/smallfooter.tsx
@@ -37,6 +37,13 @@ export default function Smallfooter({}: any) {
 									<path d="M41,4H9C6.24,4,4,6.24,4,9v32c0,2.76,2.24,5,5,5h32c2.76,0,5-2.24,5-5V9C46,6.24,43.76,4,41,4z M17,20v19h-6V20H17z M11,14.47c0-1.4,1.2-2.47,3-2.47s2.93,1.07,3,2.47c0,1.4-1.12,2.53-3,2.53C12.2,17,11,15.87,11,14.47z M39,39h-6c0,0,0-9.26,0-10 c0-2-1-4-3.5-4.04h-0.08C27,24.96,26,27.02,26,29c0,0.91,0,10,0,10h-6V20h6v2.56c0,0,1.93-2.56,5.81-2.56 c3.97,0,7.19,2.73,7.19,8.26V39z"></path>
 								</svg>
 							</a>
+
+							<a className="shrink-0 rounded-lg p-3 hover:bg-slate-3 active:bg-slate-5 transition-colors w-12" href="https://www.youtube.com/@ipBuyersAgency" target="_blank">
+								<span className="sr-only">YouTube</span>
+								<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="34" height="34" viewBox="0 0 24 24">
+									<path d="M21,5c0,0-3-1-9-1S3,5,3,5s-1,3-1,7s1,7,1,7s3,1,9,1s9-1,9-1s1-3,1-7S21,5,21,5z M10,15.464V8.536L16,12L10,15.464z"></path>
+								</svg>
+							</a>
 						</div>
 					</div>
 					<div className="flex flex-col justify-between gap-x-12 gap-y-4 text-center md:flex-row">
