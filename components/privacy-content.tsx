6856aa2236c74eb05cb27fa18e0938387dbb0dff
Prasada Indukuri <prasada.indukuri_migration@specialized.com>
2024-09-06 14:33:08 +1000

    css cleanup

--- components/privacy-content.tsx
+++ components/privacy-content.tsx
@@ -4,7 +4,7 @@ import './privacy-content.css';
 export default function PrivacyContent({}: any) {
   return (
 	<>
-		<div className="overflow-hidden">
+		<div className="privacy-content overflow-hidden">
 			<div className="prose mx-auto max-w-screen-md px-6 py-10 lg:prose-lg md:pb-24 lg:py-16 body">
 				<h1>Privacy Policy</h1>
 				<p><strong>1. Introduction</strong></p>
