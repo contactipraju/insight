dae694713746521b423ee1a3a405f4b4091b79c7
Prasada Indukuri <prasada.indukuri_migration@specialized.com>
2024-09-06 16:56:06 +1000

    adding join-news-letter inputs to footer

--- components/smallfooter.tsx
+++ components/smallfooter.tsx
@@ -1,15 +1,22 @@
 import SocialMediaLinks from "./social-media-links";
+import JoinNewsLetter from "./join-news-letter";
 
 export default function Smallfooter({}: any) {
 	return (
-		<div className="sticky bottom-0  overflow-hidden">
-			<div className="relative grid h-screen max-h-[800px] min-h-0 place-items-center overflow-y-auto overflow-x-hidden sm:pt-32 sm:pb-16 md:max-h-[1200px] md:py-48 lg:py-60">
-				<div className="mx-auto flex max-w-5xl flex-col gap-4 px-6">
+		<div className="sticky bottom-0 overflow-hidden">
+			<div className="relative grid h-screen max-h-[800px] min-h-0 place-items-center overflow-y-auto overflow-x-hidden sm:pt-32 sm:pt-16 md:max-h-[1200px] md:pt-48 lg:pt-60">
+				<div className="mx-auto flex flex-col gap-4 px-6">
 					<div className="flex flex-col items-center justify-between gap-1 md:flex-row md:gap-6">
 						<div className="left">
 							<SocialMediaLinks/>
-							<p className="text-center text-sm leading-tight text-sand-12 md:text-left">© 2024 InvestorPro. All rights reserved. <a className="underline hover:text-black" href="/privacy">Privacy policy</a></p>
-							<p className="text-center text-xs leading-tight text-sand-12 md:text-left">Made by iCube Technologies Pvt Ltd</p>
+							<div className="details pl-4">
+								<p className="text-center text-sm leading-tight text-sand-12 md:text-left">© 2024 InvestorPro. All rights reserved. <a className="underline" href="/privacy">Privacy policy</a></p>
+								<p className="text-center text-xs leading-tight text-sand-12 md:text-left">Made by iCube Technologies Pvt Ltd</p>
+							</div>
+						</div>
+
+						<div className="right">
+							<JoinNewsLetter/>
 						</div>
 					</div>
 				</div>
