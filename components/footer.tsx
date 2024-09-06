299bef1597ff47e7f00d5ca4bc45909c22ba71ae
Prasada Indukuri <prasada.indukuri_migration@specialized.com>
2024-09-06 15:22:10 +1000

    cleanup - removing unnecessary code and content

--- components/footer.tsx
+++ components/footer.tsx
@@ -16,12 +16,6 @@ export default function Footer({
 		<footer className="sticky bottom-0  overflow-hidden">
     		<div className="relative grid h-screen max-h-[800px] min-h-0 place-items-center overflow-y-auto overflow-x-hidden bg-blob-2 sm:pt-32 sm:pb-16 md:max-h-[1200px] md:py-48 lg:py-60">
 				<div className="relative isolate z-40 mx-auto h-96 max-h-screen max-w-xl px-6 lg:px-8">
-					<div>
-						<a className="group text-3xl font-bold" href="/">
-							<span className="font-bold">InvestorPro</span>
-						</a>
-						<span className="ml-2 text-xl">Buyer's Agency</span>
-					</div>
 					<p className="mt-4 text-2xl font-bold sm:mt-10 sm:text-5xl md:text-3xl whitespace-pre-line">
 						{footerHeadline}
 					</p>
