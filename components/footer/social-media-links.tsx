f5900ecff7438bd4aa5edad0ca12c1a564f631bb
Prasada Indukuri <prasada.indukuri_migration@specialized.com>
2024-09-09 15:41:36 +1000

    contact form using react-form-renderer

--- components/footer/social-media-links.tsx
+++ components/footer/social-media-links.tsx
@@ -2,35 +2,35 @@
 export default function SocialMediaLinks({}: any) {
 	return (
 		<div className="flex gap-2">
-			<a className="shrink-0 rounded-lg p-3 hover:bg-slate-3 active:bg-slate-5 transition-colors w-12" href="https://www.facebook.com/profile.php?id=61564307046162" target="_blank">
+			<a className="shrink-0 rounded-lg p-1 hover:bg-slate-3 active:bg-slate-5 transition-colors w-12" href="https://www.facebook.com/profile.php?id=61564307046162" target="_blank">
 				<span className="sr-only">Facebook</span>
 				<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="33" height="33" viewBox="0 0 24 24">
 					<path d="M17,3H7C4.791,3,3,4.791,3,7v10c0,2.209,1.791,4,4,4h5.621v-6.961h-2.343v-2.725h2.343V9.309 c0-2.324,1.421-3.591,3.495-3.591c0.699-0.002,1.397,0.034,2.092,0.105v2.43h-1.428c-1.13,0-1.35,0.534-1.35,1.322v1.735h2.7 l-0.351,2.725h-2.365V21H17c2.209,0,4-1.791,4-4V7C21,4.791,19.209,3,17,3z"></path>
 				</svg>
 			</a>
 
-			<a className="shrink-0 rounded-lg p-3 hover:bg-slate-3 active:bg-slate-5 transition-colors w-12" href="https://www.instagram.com/investorpro.ba" target="_blank">
+			<a className="shrink-0 rounded-lg p-1 hover:bg-slate-3 active:bg-slate-5 transition-colors w-12" href="https://www.instagram.com/investorpro.ba" target="_blank">
 				<span className="sr-only">Instagram</span>
 				<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="32" height="32" viewBox="0 0 30 30">
 					<path d="M 9.9980469 3 C 6.1390469 3 3 6.1419531 3 10.001953 L 3 20.001953 C 3 23.860953 6.1419531 27 10.001953 27 L 20.001953 27 C 23.860953 27 27 23.858047 27 19.998047 L 27 9.9980469 C 27 6.1390469 23.858047 3 19.998047 3 L 9.9980469 3 z M 22 7 C 22.552 7 23 7.448 23 8 C 23 8.552 22.552 9 22 9 C 21.448 9 21 8.552 21 8 C 21 7.448 21.448 7 22 7 z M 15 9 C 18.309 9 21 11.691 21 15 C 21 18.309 18.309 21 15 21 C 11.691 21 9 18.309 9 15 C 9 11.691 11.691 9 15 9 z M 15 11 A 4 4 0 0 0 11 15 A 4 4 0 0 0 15 19 A 4 4 0 0 0 19 15 A 4 4 0 0 0 15 11 z"></path>
 				</svg>
 			</a>
 
-			<a className="shrink-0 rounded-lg p-3 hover:bg-slate-3 active:bg-slate-5 transition-colors w-12" href="https://x.com/investorpro_ba" target="_blank">
+			<a className="shrink-0 rounded-lg p-1 hover:bg-slate-3 active:bg-slate-5 transition-colors w-12" href="https://x.com/investorpro_ba" target="_blank">
 				<span className="sr-only">X (Twitter)</span>
 				<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0 0 50 50">
 					<path d="M 11 4 C 7.134 4 4 7.134 4 11 L 4 39 C 4 42.866 7.134 46 11 46 L 39 46 C 42.866 46 46 42.866 46 39 L 46 11 C 46 7.134 42.866 4 39 4 L 11 4 z M 13.085938 13 L 21.023438 13 L 26.660156 21.009766 L 33.5 13 L 36 13 L 27.789062 22.613281 L 37.914062 37 L 29.978516 37 L 23.4375 27.707031 L 15.5 37 L 13 37 L 22.308594 26.103516 L 13.085938 13 z M 16.914062 15 L 31.021484 35 L 34.085938 35 L 19.978516 15 L 16.914062 15 z"></path>
 				</svg>
 			</a>
 
-			<a className="shrink-0 rounded-lg p-3 hover:bg-slate-3 active:bg-slate-5 transition-colors w-12" href="https://www.linkedin.com/company/investorproba" target="_blank">
+			<a className="shrink-0 rounded-lg p-1 hover:bg-slate-3 active:bg-slate-5 transition-colors w-12" href="https://www.linkedin.com/company/investorproba" target="_blank">
 				<span className="sr-only">LinkedIn</span>
 				<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0 0 50 50">
 					<path d="M41,4H9C6.24,4,4,6.24,4,9v32c0,2.76,2.24,5,5,5h32c2.76,0,5-2.24,5-5V9C46,6.24,43.76,4,41,4z M17,20v19h-6V20H17z M11,14.47c0-1.4,1.2-2.47,3-2.47s2.93,1.07,3,2.47c0,1.4-1.12,2.53-3,2.53C12.2,17,11,15.87,11,14.47z M39,39h-6c0,0,0-9.26,0-10 c0-2-1-4-3.5-4.04h-0.08C27,24.96,26,27.02,26,29c0,0.91,0,10,0,10h-6V20h6v2.56c0,0,1.93-2.56,5.81-2.56 c3.97,0,7.19,2.73,7.19,8.26V39z"></path>
 				</svg>
 			</a>
 
-			<a className="shrink-0 rounded-lg p-3 hover:bg-slate-3 active:bg-slate-5 transition-colors w-12" href="https://www.youtube.com/@ipBuyersAgency" target="_blank">
+			<a className="shrink-0 rounded-lg p-1 hover:bg-slate-3 active:bg-slate-5 transition-colors w-12" href="https://www.youtube.com/@ipBuyersAgency" target="_blank">
 				<span className="sr-only">YouTube</span>
 				<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="34" height="34" viewBox="0 0 24 24">
 					<path d="M21,5c0,0-3-1-9-1S3,5,3,5s-1,3-1,7s1,7,1,7s3,1,9,1s9-1,9-1s1-3,1-7S21,5,21,5z M10,15.464V8.536L16,12L10,15.464z"></path>
--- pages/contactus.tsx
+++ pages/contactus.tsx
@@ -1,12 +1,10 @@
-// Keystatic
-import FooterBottom from "../components/footer/footer-bottom";
-
 // Page section components
 import Head from "../components/head";
 import Navbar from "../components/navbar";
 import ContactForm from "../components/contact-form";
+import FooterBottom from "../components/footer/footer-bottom";
 
-export default function Contactus({ }: any) {
+export default function Contactus({}: any) {
 	return (
 		<>
 			<Head />
@@ -16,11 +14,16 @@ export default function Contactus({ }: any) {
 			</div>
 
 			<footer className="sticky bottom-0 overflow-hidden">
-				<div className="relative grid h-screen max-h-[800px] min-h-0 place-items-center overflow-y-auto overflow-x-hidden bg-blob-2 sm:pt-32 sm:pb-16 md:max-h-[1200px] md:py-48 lg:py-60">
+				<div className="relative grid h-screen max-h-[800px] min-h-0 place-items-center overflow-y-auto overflow-x-hidden bg-blob-2 sm:pt-32 sm:pb-16 md:max-h-[1200px] md:py-48 lg:py-60 pt-16">
 					<div className="relative isolate z-40 mx-auto h-96 max-h-screen max-w-xl px-6 lg:px-8">
-						<ContactForm />
-					</div>
+						<div className="enquiries">
+							<ContactForm />
+						</div>
 
+						<div className="mt-4">
+							Or call us at: <span>&#9742;</span>: +61 432 908 730
+						</div>
+					</div>
 					<FooterBottom/>
 				</div>
 			</footer>
