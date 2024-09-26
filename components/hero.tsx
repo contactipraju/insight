449df7e3e7e565cb2eeb1fc1a436270d73c49d2a
Prasada Indukuri <prasada.indukuri_migration@specialized.com>
2024-09-27 08:11:35 +1000

    cleanup around hero image

--- components/hero.tsx
+++ components/hero.tsx
@@ -3,47 +3,49 @@ import Image from "next/image";
 import heroImage from "../public/images/hero-image.png";
 
 export type HeroProps = {
-  heroHeadline: string;
-  heroIntroText: string;
+	heroHeadline: string;
+	heroIntroText: string;
 };
 
 type ComponentProps = {
-  data: HeroProps;
+	data: HeroProps;
 };
 
 export default function Hero({
-  data: { heroHeadline, heroIntroText },
+	data: { heroHeadline, heroIntroText },
 }: ComponentProps) {
-  return (
-    <section className="relative overflow-hidden bg-gradient-to-br from-white/50 to-transparent">
-      {/* Blob 2 */}
+	return (
+		<section className="relative overflow-hidden bg-gradient-to-br from-white/50 to-transparent">
+		{/* Blob 2 */}
 
-      <div className="relative z-10 mx-auto grid max-w-5xl py-16 px-6 sm:py-24 md:grid-cols-2 md:pt-28 md:pb-8 lg:px-8">
-        <div className="absolute bottom-0 -right-24">
-          <div className="h-[300px] w-[400px] bg-[radial-gradient(50%_50%_at_50%_50%,#cfbcdc_0%,rgba(207,188,220,0.66)_24.48%,rgba(239,238,243,0)_100%)] opacity-60 mix-blend-color-burn [animation-delay:2s]"></div>
-        </div>
+			<div className="relative z-10 mx-auto grid max-w-5xl py-16 px-6 sm:py-24 md:grid-cols-2 md:pt-28 md:pb-8 lg:px-8">
+				<div className="absolute bottom-0 -right-24">
+					<div className="h-[300px] w-[400px] bg-[radial-gradient(50%_50%_at_50%_50%,#cfbcdc_0%,rgba(207,188,220,0.66)_24.48%,rgba(239,238,243,0)_100%)] opacity-60 mix-blend-color-burn [animation-delay:2s]"></div>
+				</div>
 
-        <Image
-          src={heroImage}
-          alt=""
-          className="absolute right-0 bottom-0 hidden max-w-[60%] md:block"
-        />
+				<Image
+					src={heroImage}
+					alt=""
+					className="absolute right-0 bottom-0 hidden max-w-[60%] md:block"
+				/>
 
-        {/* Mobile image */}
-        <Image
-          src={heroImage}
-          alt=""
-          className="mx-auto w-[85%] max-w-[80%] md:hidden"
-        />
-        <div>
-          <h1 className="mt-2 text-3xl font-bold sm:text-4xl md:mt-4 md:text-5xl lg:text-5xl whitespace-pre-line">
-            {heroHeadline}
-          </h1>
-          <p className="mt-6 sm:mt-10 sm:text-base sm:leading-6 whitespace-pre-line">
-            {heroIntroText}
-          </p>
-        </div>
-      </div>
-    </section>
-  );
+				{/* Mobile image */}
+				<Image
+					src={heroImage}
+					alt=""
+					className="mx-auto w-[85%] max-w-[80%] md:hidden"
+				/>
+
+				<div>
+					<h1 className="mt-2 text-3xl font-bold sm:text-4xl md:mt-4 md:text-5xl lg:text-5xl whitespace-pre-line">
+						{heroHeadline}
+					</h1>
+
+					<p className="mt-6 sm:mt-10 sm:text-base sm:leading-6 whitespace-pre-line">
+						{heroIntroText}
+					</p>
+				</div>
+			</div>
+		</section>
+	);
 }
