1db3ac2cd3e6f7e234da54c8e0fe243cfe25b101
Prasada Indukuri <prasada.indukuri_migration@specialized.com>
2024-09-27 08:09:03 +1000

    reduced font-size for CopyRight info

--- components/footer/copyright-details.tsx
+++ components/footer/copyright-details.tsx
@@ -1,9 +1,9 @@
 
 export default function CopyrightDetails({}: any) {
 	return (
-		<div className="details">
-			<p className="text-center text-sm leading-tight text-sand-12 md:text-center">© 2024 InvestorPro. All rights reserved.</p>
-			<p className="text-center text-sm leading-tight text-sand-12 md:text-center"><a className="underline" href="/privacy">Privacy policy</a></p>
+		<div className="details text-center text-xs leading-tight text-sand-12 md:text-center">
+			<p>© 2024 InvestorPro. All rights reserved.</p>
+			<p><a className="underline" href="/privacy">Privacy policy</a></p>
 			{/* <p className="text-center text-xs leading-tight text-sand-12 md:text-center">Made by iCube Technologies Pvt Ltd</p> */}
 		</div>
 	);
