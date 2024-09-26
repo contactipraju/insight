d51c340815a704127fec1f3013757f6d4a961532
Prasada Indukuri <prasada.indukuri_migration@specialized.com>
2024-09-27 08:09:37 +1000

    changed footer bg color to light orange

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -15,7 +15,7 @@ export default function Footer({
 }: ComponentProps) {
 	return (
 		<footer className="sticky bottom-0 overflow-hidden">
-			<div className="relative bg-blob-2 grid h-screen max-h-[1200px] min-h-0 overflow-y-auto overflow-x-hidden sm:pt-32 sm:pt-16 md:max-h-[1200px] md:pt-48 lg:pt-60">
+			<div className="relative bg-orange-100 grid h-screen max-h-[1200px] min-h-0 overflow-y-auto overflow-x-hidden sm:pt-32 sm:pt-16 md:max-h-[1200px] md:pt-48 lg:pt-60">
 				<div className="mx-auto flex flex-col gap-2 px-6 pb-8">
 					<FooterContent data={{footerHeadline, footerText}} />
 				</div>
