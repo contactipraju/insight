aec33a4611c41b9a133f34b400e0519f8317adea
Prasada Indukuri <prasada.indukuri_migration@specialized.com>
2024-11-01 09:15:22 +1100

    using brand fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import type { AppProps } from "next/app";
 
 import "../styles/tailwind.css";
+import "../public/css/common.css";
+import "../public/css/zodiak.css";
+import "../public/css/plus-jakarta-sans.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
