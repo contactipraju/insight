4971571ce7c9dd17852e771896d4777a2e65ee74
Prasada Indukuri <prasada.indukuri_migration@specialized.com>
2024-11-23 13:18:45 +1100

    design changes to deals

--- components/projects/Project.tsx
+++ components/projects/Project.tsx
@@ -5,52 +5,56 @@ const Project = ({project}) => {
 	return (
 		<div className="project">
 			<div className="details">
-				<div className='title'>
-					<div className="type">{project.type}</div>
+				{/*
+					<div className='title'>
+						<div className="type">{project.type}</div>
 
-					<div className="location" style={{ backgroundImage: `url(images/portfolio/location.png)` }}>
-						{project.location}
+						<div className="location" style={{ backgroundImage: `url(images/portfolio/location.png)` }}>
+							{project.location}
+						</div>
 					</div>
-				</div>
-
-				<div className="name">{project.name}</div>
+				*/}
 
 				<div className="images" style={{ backgroundImage: `url(${project.images![0]})` }}></div>
 
-				<div className="features">
-					<ul>
-						{project.features.map(item => {
-							return <li key={item}>{item}</li>;
-						})}
-					</ul>
-				</div>
+				<div className='data'>
+					<div className="name">{project.name}</div>
 
-				<div className="financials">
-					<table>
-						<thead>
-							<tr>
-								<th className="text">Financials</th>
-								<th className="amount">(AUD)</th>
-							</tr>
-						</thead>
-						<tbody>
-						{
-							Object.keys(project.financials).map((key: any, i: any) => (
-								<tr key={i}>
-									<td className="text">{key.replace(/_/g,' ')}</td>
-									<td className="amount">{project.financials[key]}</td>
+					<div className="financials">
+						<table>
+							{/* <thead>
+								<tr>
+									<th className="text">Financials</th>
+									<th className="amount">(AUD)</th>
 								</tr>
-							))}
-						</tbody>
-					</table>
-				</div>
+							</thead> */}
+							<tbody>
+							{
+								Object.keys(project.financials).map((key: any, i: any) => (
+									<tr key={i}>
+										<td className="text">{key.replace(/_/g,' ')}</td>
+										<td className="amount">${project.financials[key]}</td>
+									</tr>
+								))}
+							</tbody>
+						</table>
+					</div>
 
-				<div className="success">
-					<ul>
-						{project.success.map(item => {
-							return <li key={item}>{item}</li>;
-						})}
-					</ul>
+					<div className="features">
+						<ul>
+							{project.features.map(item => {
+								return <li key={item}>{item}</li>;
+							})}
+						</ul>
+					</div>
+
+					<div className="success">
+						<ul>
+							{project.success.map(item => {
+								return <li key={item}>{item}</li>;
+							})}
+						</ul>
+					</div>
 				</div>
 			</div>
 		</div>
