407736a58d80c2d103da0bfbf02f69d83a778bce
Prasada Indukuri <prasada.indukuri_migration@specialized.com>
2024-12-07 22:14:32 +1100

    setting up Google Analytics

--- components/head.tsx
+++ components/head.tsx
@@ -1,43 +1,42 @@
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import NextHead from "next/head";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const meta = {
-  title: "InvestorPro Buyer's Agency in Australia | Expert Property Buying Services",
-  description:
-    "Top Buyer's Agency in Australia offering expert property buying services. Find the best Buyer's Agent for your next home or investment.",
-  imagePath: "/images/seo-image.png",
+	title: "InvestorPro Buyer's Agency in Australia | Expert Property Buying Services",
+	description: "Best Buyer's Agency in Australia offering expert property buying services for your next home or investment.",
+	imagePath: "/images/seo-image.png"
 };
 
 export default function Head() {
-  // Get correct domain to pass it to SEO image
-  const router = useRouter();
-  const [rootUrl, setRootUrl] = useState("");
-  useEffect(() => {
-    setRootUrl(window.location.origin);
-  }, [router.pathname]);
+	// Get correct domain to pass it to SEO image
+	const router = useRouter();
+	const [rootUrl, setRootUrl] = useState("");
+	useEffect(() => {
+		setRootUrl(window.location.origin);
+	}, [router.pathname]);
 
-  return (
-    <NextHead>
-      <title>{meta.title}</title>
-      <meta name="description" content={meta.description} />
-      <meta name="robots" content="index,follow" />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site" content="@site" />
-      <meta name="twitter:creator" content="@thekeystatic" />
-      <meta property="og:title" content={meta.title} />
-      <meta property="og:description" content={meta.description} />
-      <meta
-        property="og:image:alt"
-        content="cover image for the Keystatic Marketing Landing Page Template"
-      />
-      <meta property="og:image:type" content="image/png" />
-      <meta property="og:image:width" content="1152" />
-      <meta property="og:image:height" content="780" />
-      <meta property="og:site_name" content={meta.title} />
-      <meta property="og:url" content={rootUrl} />
-      <meta property="og:image" content={rootUrl + meta.imagePath} />
-      <link rel="icon" href="/favicon.ico" />
-    </NextHead>
-  );
+	return (
+	    <NextHead>
+			<GoogleAnalytics gaId="G-T0XZY5BLB0" />
+
+			<title>{meta.title}</title>
+			<meta name="description" content={meta.description} />
+			<meta name="robots" content="index,follow" />
+			<meta name="twitter:card" content="summary_large_image" />
+			<meta name="twitter:site" content="@site" />
+			<meta name="twitter:creator" content="@thekeystatic" />
+			<meta property="og:title" content={meta.title} />
+			<meta property="og:description" content={meta.description} />
+			<meta property="og:image:alt" content="cover image for the Keystatic Marketing Landing Page Template" />
+			<meta property="og:image:type" content="image/png" />
+			<meta property="og:image:width" content="1152" />
+			<meta property="og:image:height" content="780" />
+			<meta property="og:site_name" content={meta.title} />
+			<meta property="og:url" content={rootUrl} />
+			<meta property="og:image" content={rootUrl + meta.imagePath} />
+			<link rel="icon" href="/favicon.ico" />
+		</NextHead>
+	);
 }
