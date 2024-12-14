e9784d2e544d2f6dc5c0ff300e198e626cf2a6a7
Prasada Indukuri <prasada.indukuri_migration@specialized.com>
2024-12-14 19:54:30 +1100

    Contact us zoho-form integrated

--- components/contact-form.tsx
+++ components/contact-form.tsx
@@ -1,59 +1,12 @@
-import FormRenderer from '@data-driven-forms/react-form-renderer/form-renderer';
-import componentTypes from '@data-driven-forms/react-form-renderer/component-types';
-import FormTemplate from '@data-driven-forms/mui-component-mapper/form-template';
-import { componentMapper } from '@data-driven-forms/mui-component-mapper';
-
 export default function ContactForm({}: any) {
-	const schema = {
-		fields: [
-			{
-				component: componentTypes.TEXT_FIELD,
-				name: 'name',
-				label: 'Your name',
-				isRequired: true,
-				validate: [{ type: 'required' }]
-			},
-			{
-				component: componentTypes.TEXT_FIELD,
-				name: 'email',
-				label: 'Email',
-				isRequired: true,
-				validate: [
-					{ type: 'required' },
-					{
-						type: 'pattern',
-						pattern: '[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,}$',
-						message: 'Not valid email'
-					}
-				]
-			},
-			{
-				component: componentTypes.TEXT_FIELD,
-				name: 'phone',
-				label: 'Phone number'
-			},
-			{
-				component: componentTypes.TEXTAREA,
-				name: 'message',
-				label: 'Message'
-			},
-			{
-				component: componentTypes.CHECKBOX,
-				name: 'newsletters',
-				label: 'I want to receive newsletter',
-				checked: true
-			}
-		]
-	};
+
+	const url = 'https://forms.zohopublic.com.au/investorproptyltd1/form/Contactus/formperma/JZOUEjtVIRr2TKPhlB5eORxbXXJSV4jeqOdxTLapclI';
 
 	return (
 		<>
-			<FormRenderer
-				schema={schema}
-				componentMapper={componentMapper}
-				FormTemplate={FormTemplate}
-				onSubmit={console.log}
-			/>
+			<div className="h-100">
+				<iframe src={url} height={830} width={650}/>
+			</div>
 		</>
 	);
 }
--- pages/contactus.tsx
+++ pages/contactus.tsx
@@ -1,14 +1,23 @@
 import DefaultHeaderAndBody from "../components/defaultHeaderAndBody";
-import DefaultFooter from "../components/defaultFooter";
+import ContactForm from "../components/contact-form";
+import FooterBottom from "../components/footer/footer-bottom";
 
 export default function Contactus({}: any) {
 	return (
 		<>
 			<DefaultHeaderAndBody>
-				<div className="content pt-20"></div>
+				<div className="content pt-20">
+					<div className="relative grid h-screen min-h-0 place-items-center overflow-y-auto overflow-x-hidden sm:pt-2 md:max-h-[1200px] pt-24">
+						<div className="relative isolate z-40 mx-auto h-70 max-h-screen max-w-3xl px-6">
+							<div className="enquiries">
+								<ContactForm />
+							</div>
+						</div>
+					</div>
+				</div>
 			</DefaultHeaderAndBody>
 
-			<DefaultFooter />
+			<FooterBottom />
 		</>
 	);
 }
