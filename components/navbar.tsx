01ef0c4ec9e1ee5a01ad00e4fcb1034f66d4cb6d
Prasada Indukuri <prasada.indukuri_migration@specialized.com>
2025-01-05 00:52:30 -0800

    Contact us button on header has Brand Orange color as bg, with black text in semibold, and reduced the height of the header

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,7 +5,7 @@ import LogoLink from "./logo-link";
 export default function Navbar() {
 	return (
 		<div className="relative">
-			<header className="absolute inset-x-0 z-20 sm:fixed sm:h-20 bg-black text-white py-5 sm:py-8 sm:backdrop-blur">
+			<header className="absolute inset-x-0 z-20 sm:fixed sm:h-16 bg-black text-white py-5 sm:py-8 sm:backdrop-blur">
 				<div className="mx-auto flex h-full max-w-6xl items-center justify-between px-6 lg:px-8">
 					<LogoLink />
 
@@ -19,7 +19,7 @@ export default function Navbar() {
 
 						<div className="links hidden sm:flex ml-8">
 							{/* <Link href="/keystatic" className="rounded-full px-5 py-2.5 font-medium hidden gap-10 sm:inline"> Admin </Link> */}
-							<Link rel="noopener noreferrer" target="_blank" href="https://forms.investorpro.com.au/investorproptyltd1/form/Contactus/formperma/JZOUEjtVIRr2TKPhlB5eORxbXXJSV4jeqOdxTLapclI" className="rounded-full bg-black px-5 py-2.5 font-medium text-white bg-gray-900 hover:bg-gray-700 whitespace-nowrap"> Contact us </Link>
+							<Link rel="noopener noreferrer" target="_blank" href="https://forms.investorpro.com.au/investorproptyltd1/form/Contactus/formperma/JZOUEjtVIRr2TKPhlB5eORxbXXJSV4jeqOdxTLapclI" className="rounded-full bg-black px-5 py-2 font-semibold text-white btn-orange whitespace-nowrap"> Contact us </Link>
 						</div>
 					</nav>
 				</div>
