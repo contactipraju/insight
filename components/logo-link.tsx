633bffebad57bd5afb201f241d023f61442fd2f3
Prasada Indukuri <prasada.indukuri_migration@specialized.com>
2025-02-07 22:12:15 +1100

    moved images

--- components/logo-link.tsx
+++ components/logo-link.tsx
@@ -2,8 +2,9 @@ import Link from "next/link";
 
 import Image from "next/image";
 
-import logoImage from "../public/images/logo-image.png";
-
+import logoImage from "../public/images/investorpro/logo-image.png";
+import logoImageBlack from "../public/images/investorpro/logo-image-black.png";
+import sowjanyaProfile from "../public/images/investorpro/sowjanya-profile.jpeg";
 
 export default function LogoLink() {
 	return (
@@ -11,6 +12,22 @@ export default function LogoLink() {
 			<div>
 				<Image
 					src={logoImage}
+					alt=""
+					className="max-w-[60%] md:max-w-[50%] lg:max-w-[40%] md:block"
+				/>
+			</div>
+
+			<div className="hidden">
+				<Image
+					src={logoImageBlack}
+					alt=""
+					className="max-w-[60%] md:max-w-[50%] lg:max-w-[40%] md:block"
+				/>
+			</div>
+
+			<div className="hidden">
+				<Image
+					src={sowjanyaProfile}
 					alt=""
 					className="max-w-[60%] md:max-w-[50%] lg:max-w-[40%] md:block"
 				/>
