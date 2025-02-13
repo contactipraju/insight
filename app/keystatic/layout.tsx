e418b4ad493c37df63e26f8039e3ffb4ce9f39d9
Prasada Indukuri <prasada.indukuri_migration@specialized.com>
2025-02-14 10:07:57 +1100

    Enabling vercel analytics

--- app/keystatic/layout.tsx
+++ app/keystatic/layout.tsx
@@ -1,5 +1,8 @@
 import KeystaticApp from "./keystatic";
-import { GoogleAnalytics } from '@next/third-parties/google'
+
+import { Analytics } from '@vercel/analytics/next';
+
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 export const metadata = {
 	title: "Keystatic: Admin UI",
@@ -12,6 +15,7 @@ export default function RootLayout() {
 
 			<body>
 				<KeystaticApp />
+				<Analytics />
 				<GoogleAnalytics gaId="G-T0XZY5BLB0" />
 			</body>
 		</html>
